e24240cf8658952c9c6110659f3431ae381f7ef8
just-sabyr <bazarymbetovsabyr@gmail.com>
2024-09-15 18:37:49 +0300

    First Commit

---
+++ blog/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Post, Comment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']     
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author'] # A lookup widget instead of drop-down list
+    date_hierarchy = 'publish' # Navigation links through dates
+    ordering = ['status', 'publish']
+    show_facets = admin.ShowFacets.ALWAYS # Show counts specific to filters
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
