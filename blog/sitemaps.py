01d8e36bf06d98f68864346867b8ed36dafeefdd
just-sabyr <bazarymbetovsabyr@gmail.com>
2024-09-17 08:41:21 +0300

    added tags to sitemaps.xml

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -1,5 +1,8 @@
 from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+from taggit.models import Tag
 from .models import Post
+
 
 
 class PostSitemap(Sitemap):
@@ -12,4 +15,15 @@ class PostSitemap(Sitemap):
     def lastmod(self, obj):
         return obj.updated
     
+
+class TagSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.8
+
+    def items(self):
+        # Return all tags used in the blog
+        return Tag.objects.all()
     
+    def location(self, item):
+        # Generate the URL for each tag
+        return reverse('blog:post_list_by_tag', args=[item.slug])
